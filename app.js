d47f28cf9a01c3f1fadaa1f84c77d333668ec466
Om Chevli <omchevli@gmail.com>
2020-12-23 00:13:59 +0530

    Callback Chained along with user input

--- app.js
+++ app.js
@@ -2,22 +2,19 @@ const request = require('request')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-//
-// Goal: Create a reusable function for getting the forecast
-//
-// 1. Setup the "forecast" function in utils/forecast.js
-// 2. Require the function in app.js and call it as shown below
-// 3. The forecast function should have three potential calls to callback:
-//    - Low level error, pass string for error
-//    - Coordinate error, pass string for error
-//    - Success, pass forecast string for data (same format as from before)
+const city = process.argv[2]
+if (city === null || city === undefined || city === "") {
+    console.log('Please Enter City.')
+} else {
+    const logic = geocode(city, (error, data) => {
+        if (error)
+            return console.log(error)
 
-forecast(27.2046, 77.4977, (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-})
-
-// geocode('Surat Gujarat', (error, data) => {
-//     console.log(error)
-//     console.log(data)
-// })
+        forecast(data.lon, data.lat, (error, forecastData) => {
+            if (error)
+                return console.log(error)
+            console.log(data.location)
+            console.log(forecastData)
+        })
+    })
+}
