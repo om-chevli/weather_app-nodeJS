c70c17a23f98ca0354c2604386177da8c8484ef4
Om Chevli <omchevli@gmail.com>
2020-12-22 23:35:16 +0530

    forecast module

--- app.js
+++ app.js
@@ -1,27 +1,23 @@
 const request = require('request')
 const geocode = require('./utils/geocode')
-const weatherstack_url = 'http://api.weatherstack.com/current?access_key=c99ccfaabc20c162b579559fd619d40a&query=37.8261,-122.4233&units=m'
+const forecast = require('./utils/forecast')
 
+//
+// Goal: Create a reusable function for getting the forecast
+//
+// 1. Setup the "forecast" function in utils/forecast.js
+// 2. Require the function in app.js and call it as shown below
+// 3. The forecast function should have three potential calls to callback:
+//    - Low level error, pass string for error
+//    - Coordinate error, pass string for error
+//    - Success, pass forecast string for data (same format as from before)
 
-// 37.8261,-122.4233
-
-// request({ url: weatherstack_url, json: true }, (error, response) => {
-//     try {
-//         const w_description = response.body.current.weather_descriptions[0]
-//         const current_temp = response.body.current.temperature
-//         const apparent_temp = response.body.current.feelslike
-//         console.log(w_description, "atmosphere. It is", current_temp, "degrees. It feels like", apparent_temp, "out there.")
-//     } catch {
-//         if (error) {
-//             console.log('Unable to connect to weather services. Check your internet connectivity.')
-//         } else if (response.body.error) {
-//             console.log('Unable to find the location.')
-//         }
-//     }
-
-// })
-
-geocode('Surat Gujarat', (error, data) => {
-    console.log(error)
-    console.log(data)
+forecast(27.2046, 77.4977, (error, data) => {
+    console.log('Error', error)
+    console.log('Data', data)
 })
+
+// geocode('Surat Gujarat', (error, data) => {
+//     console.log(error)
+//     console.log(data)
+// })
